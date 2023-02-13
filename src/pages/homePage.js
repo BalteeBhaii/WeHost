005782d87b1feb84447c4eba777664f8d9dfd6d2
Nahoor07 <nahoor98@gmail.com>
2023-02-14 00:13:34 +0500

    footer design ready

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import Header from '../components/header/header';
-import Destination from '../components/destination/destination';
-import LocationModal from '../components/LocationModal/LocationModal';
-import GuestModal from '../components/GuestModal/GuestModal';
-import FilterModal from '../components/Filter/filter';
+import React from "react";
+import Header from "../components/header/header";
+import Destination from "../components/destination/destination";
+import LocationModal from "../components/LocationModal/LocationModal";
+import GuestModal from "../components/GuestModal/GuestModal";
+import FilterModal from "../components/Filter/filter";
+import Footer from "../components/footer/footer";
 
 const HomePage = () => {
-    return ( 
-        <>
-        <Header/>
-        <Destination/>
-        <LocationModal/>
-        <GuestModal/>
-        <FilterModal/>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Header />
+      <Destination />
+      <LocationModal />
+      <GuestModal />
+      <FilterModal />
+      <Footer />
+    </>
+  );
+};
+
 export default HomePage;
