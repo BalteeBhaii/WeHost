29a0cd14f4c72c2b2c6197e9523613287e225faa
Nahoor07 <nahoor98@gmail.com>
2023-02-15 16:29:36 +0500

    footer and conflicts fixed

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -9,7 +9,6 @@ import Explore from '../components/homePageComponents/explore/explore';
 import Services from '../components/homePageComponents/services/services';
 import BestPlace from '../components/homePageComponents/bestPlace/bestPlace';
 import RentalIncome from '../components/homePageComponents/rentalIncome/rentalIncome';
-import Footer from '../components/Navigation/footer/footer';
 const HomePage = () => {
     return ( 
         <>
