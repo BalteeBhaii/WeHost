96483b5c40eeed0d1074adced42f17fff83966d3
Muzzammil-Sanzar <muzzammil6435@gmail.com>
2023-02-16 11:15:33 +0500

    css files changes done

--- src/components/hotelscomponents/DesiredHotels/desiredHotels.js
+++ src/components/hotelscomponents/DesiredHotels/desiredHotels.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import './desiredHotels.css'
+import '../../../globalcss/styles.css'
 const DesiredHotels = () => {
     return (
         <>
             <div className='container mt-5 mb-3'>
                 <div className='w-100 d-flex justify-content-between align-items-center main-text-size-style'>
                     <h1 className='mb-0'>Here are the desired hotels for you</h1>
-                    <button className='btn btn-primary btn-md m-1' ><i class="bi bi-funnel me-1"></i>Filters</button>
+                    <button className='btn btn-primary filter-button-size-style btn-md m-1' ><i class="bi bi-funnel me-1"></i>Filters</button>
                 </div>
             </div>
             <div className='container mb-5'>
                 <div className='row'>
                     <div className='col-sm-11 col-md-6 col-lg-4 mb-3'>
-                        <div className="card" >
+                        <div className="card hotel-card-style" >
                             <div className='image-div'>
                                 <img className='imgSizeStyle' src="/images/cardImage.svg" width={'100%'} height={'100%'} alt="..."/>
                             </div>
@@ -40,7 +40,7 @@ const DesiredHotels = () => {
                         </div>
                     </div>
                     <div className='col-sm-11 col-md-6 col-lg-4 mb-3'>
-                        <div className="card" >
+                        <div className="card hotel-card-style" >
                             <div className='image-div'>
                                 <img className='imgSizeStyle' src="/images/cardImage.svg" width={'100%'} height={'100%'} alt="..."/>
                             </div>
@@ -60,14 +60,14 @@ const DesiredHotels = () => {
                                     <p className="card-text mb-1">Enjoy the Lima city sightseeing and explore the buetyof Lima</p>
                                     <div className='d-flex justify-content-between w-100' style={{fontSize: '0.8rem'}}>
                                         <p className='fw-semibold'><i class="bi bi-geo-alt me-1 fw-semibold"></i>Lima City</p>
-                                        <p><a><ins>Know More</ins></a></p>
+                                        <p><a href='/hotel'><ins>Know More</ins></a></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className='col-sm-11 col-md-6 col-lg-4 mb-3'>
-                        <div className="card" >
+                        <div className="card hotel-card-style" >
                             <div className='image-div'>
                                 <img className='imgSizeStyle' src="/images/cardImage.svg" width={'100%'} height={'100%'} alt="..."/>
                             </div>
@@ -87,14 +87,14 @@ const DesiredHotels = () => {
                                     <p className="card-text mb-1">Enjoy the Lima city sightseeing and explore the buetyof Lima</p>
                                     <div className='d-flex justify-content-between w-100' style={{fontSize: '0.8rem'}}>
                                         <p className='fw-semibold'><i class="bi bi-geo-alt me-1 fw-semibold"></i>Lima City</p>
-                                        <p><a><ins>Know More</ins></a></p>
+                                        <p><a href='/hotel'><ins>Know More</ins></a></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className='col-sm-11 col-md-6 col-lg-4 mb-3'>
-                        <div className="card" >
+                        <div className="card hotel-card-style" >
                             <div className='image-div'>
                                 <img className='imgSizeStyle' src="/images/cardImage.svg" width={'100%'} height={'100%'} alt="..."/>
                             </div>
@@ -114,14 +114,14 @@ const DesiredHotels = () => {
                                     <p className="card-text mb-1">Enjoy the Lima city sightseeing and explore the buetyof Lima</p>
                                     <div className='d-flex justify-content-between w-100' style={{fontSize: '0.8rem'}}>
                                         <p className='fw-semibold'><i class="bi bi-geo-alt me-1 fw-semibold"></i>Lima City</p>
-                                        <p><a><ins>Know More</ins></a></p>
+                                        <p><a href='/hotel'><ins>Know More</ins></a></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className='col-sm-11 col-md-6 col-lg-4 mb-3'>
-                        <div className="card" >
+                        <div className="card hotel-card-style" >
                             <div className='image-div'>
                                 <img className='imgSizeStyle' src="/images/cardImage.svg" width={'100%'} height={'100%'} alt="..."/>
                             </div>
@@ -141,14 +141,14 @@ const DesiredHotels = () => {
                                     <p className="card-text mb-1">Enjoy the Lima city sightseeing and explore the buetyof Lima</p>
                                     <div className='d-flex justify-content-between w-100' style={{fontSize: '0.8rem'}}>
                                         <p className='fw-semibold'><i class="bi bi-geo-alt me-1 fw-semibold"></i>Lima City</p>
-                                        <p><a><ins>Know More</ins></a></p>
+                                        <p><a href='/hotel'><ins>Know More</ins></a></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className='col-sm-11 col-md-6 col-lg-4 mb-3'>
-                        <div className="card" >
+                        <div className="card hotel-card-style" >
                             <div className='image-div'>
                                 <img className='imgSizeStyle' src="/images/cardImage.svg" width={'100%'} height={'100%'} alt="..."/>
                             </div>
@@ -168,7 +168,7 @@ const DesiredHotels = () => {
                                     <p className="card-text mb-1">Enjoy the Lima city sightseeing and explore the buetyof Lima</p>
                                     <div className='d-flex justify-content-between w-100' style={{fontSize: '0.8rem'}}>
                                         <p className='fw-semibold'><i class="bi bi-geo-alt me-1 fw-semibold"></i>Lima City</p>
-                                        <p><a><ins>Know More</ins></a></p>
+                                        <p><a href='/hotel'><ins>Know More</ins></a></p>
                                     </div>
                                 </div>
                             </div>
