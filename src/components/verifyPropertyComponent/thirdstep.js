84150045128a2e599b41a3bf38b4f980607e0a00
Nahoor07 <nahoor98@gmail.com>
2023-03-07 15:34:13 +0500

    hello

--- src/components/verifyPropertyComponent/thirdstep.js
+++ src/components/verifyPropertyComponent/thirdstep.js
@@ -1,5 +1,13 @@
 import React from 'react';
 const ThirdStep = () => {
+    const setCategory = (event) => {
+        const parentEl = event.nativeEvent.target.closest('.type-box');
+        if (!parentEl) return;
+      
+        const categoryEls = document.querySelectorAll('.type-box');
+        categoryEls.forEach((el) => el.classList.remove('active-category'));
+        parentEl.classList.add('active-category');
+    }
     return (
         <>
             <div className='container'>
@@ -7,37 +15,40 @@ const ThirdStep = () => {
                 <div className='verify-2nd-list-items row mt-4'>
                     <div className='verify-2nd-list-items-1st-column col-md-6 col-sm-12 row'>
                         <div className='col-12 col-lg-9 col-md-9 col-sm-9 col-9 verify-2nd-list-item-holder'>
-                            <div className='verify-2nd-list-item border py-3 mb-3'>
+                            <div onClick={setCategory} className='type-box verify-2nd-list-item border py-3 mb-3'>
                                 <div className="verify-2nd-list-item-main gap-4 d-flex">
                                 <img className="verify-3rd-list-item-main-icon"   src='/images/mansion.png' /> 
 
                                 <div className='row'>
                                     <p className='mb-0'>Entire place</p>
                                     <p className="entire-place-text">Offer them entire place of yours</p>
+                                    <input type={'radio'} name= 'type' className='d-none' value={'entire place'}/>
                                     </div>                              
                                 </div>
                             </div>
                         </div>
                         <div className='col-12  col-lg-9 col-md-9 col-sm-9 col-9 verify-2nd-list-item-holder'>
-                            <div className='verify-2nd-list-item border  py-3 mb-3'>
+                            <div onClick={setCategory} className='type-box verify-2nd-list-item border  py-3 mb-3'>
                             <div className="verify-2nd-list-item-main gap-4 d-flex">
                                 <img className="verify-3rd-list-item-main-icon"   src='/images/privateroom.png' /> 
 
                                 <div className='row'>
                                     <p className='mb-0'>Private Room</p>
                                     <p className="entire-place-text">Offer them single room of yours</p>
+                                    <input type={'radio'} name= 'type' className='d-none' value={'private room'}/>
                                     </div>                              
                                 </div>
                             </div>
                         </div>
                         <div className='col-12 col-lg-9 col-md-9 col-sm-9 col-9 verify-2nd-list-item-holder'>
-                            <div className='verify-2nd-list-item border  py-3 mb-3'>
+                            <div onClick={setCategory} className='type-box verify-2nd-list-item border  py-3 mb-3'>
                             <div className="verify-2nd-list-item-main gap-4 d-flex">
                                 <img className="verify-3rd-list-item-main-icon"   src='/images/sharedroom.png' /> 
 
                                 <div className='row'>
                                     <p className='mb-0'>Shared Room</p>
                                     <p className="entire-place-text">Offer them to share room</p>
+                                    <input type={'radio'} name= 'type' className='d-none' value={'Shared room'}/>
                                     </div>                              
                                 </div>
                             </div>
