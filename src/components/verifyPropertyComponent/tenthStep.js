e1ba17e678bc3686a008b11359a1520e6110c7ca
Muzzammil-Sanzar <muzzammil6435@gmail.com>
2023-03-08 16:49:55 +0500

    dropdown

--- src/components/verifyPropertyComponent/tenthStep.js
+++ src/components/verifyPropertyComponent/tenthStep.js
@@ -19,10 +19,20 @@ const TenthStep = ({id, setId}) => {
                 </div>
                 <h4 className='mt-4'>Write your description here which tell us about your house </h4>
                 <div className="input-group property-title-field">
-                <textarea className="form-control shadow-none" onChange={(e) => {setDiscription(e.target.value); setId(true)}} aria-label="With textarea" maxlength="100"></textarea>
+                    <textarea className="form-control shadow-none" onChange={(e) => {setDiscription(e.target.value); setId(true)}} aria-label="With textarea" maxlength="100"></textarea>
                 
                 </div>
                 <h4 className='mt-2'>0/100</h4>
+                <h3 className='verify-2nd-title my-3'>Which type of guest can book property?</h3>
+                <div class="dropdown">
+                    <button class="btn border border-2 dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Select Guest Type
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li>all-guests</li>
+                        <li>experienced-guests</li>
+                    </ul>
+                </div>
             </div>
         </>
     );
--- src/pages/verifyPropertyPage.js
+++ src/pages/verifyPropertyPage.js
@@ -115,8 +115,15 @@ const VerifyPropertyPage = () => {
           }
           {(page === 4) && <ForthStep />}
           {(page === 5) && <FifthStep />}
-          {(page === 6) && <SixthStep id={id} setId={setId} listingCompleteData={listingCompleteData}
-              setListingCompleteData={setListingCompleteData}/>}
+          {
+            (page === 6) && 
+            <SixthStep 
+              id={id} 
+              setId={setId} 
+              listingCompleteData={listingCompleteData}
+              setListingCompleteData={setListingCompleteData}
+            />
+          }
           {(page === 7) && <StepTwo />}
           {(page === 8) && <SeventhStep id={id} setId={setId} />}
           {(page === 9) && <EightStep id={id} setId={setId} />}
@@ -141,7 +148,7 @@ const VerifyPropertyPage = () => {
             {(page >= 1) && (
               <div className='d-flex justify-content-between'>
                 <button className='btn property-footer-button' onClick={() => { setPage(page - 1); setWidth((page - 1) * 6.66); console.log(page, width) }}><i className="bi bi-arrow-left me-1"></i>Back</button>
-                {(page < 15) && (id) || (page === 1) || (page === 4) || (page === 5) || (page === 7) || (page === 8) || (page === 10) || (page === 12) ? (
+                {(page < 15) && (id) || (page === 1) || (page === 4) || (page === 5) || (page === 7) || (page === 8) || (page === 10) || (page === 12) || (page === 6) ? (
                   <button className='btn property-footer-button' onClick={() => { setPage(page + 1); setWidth((page + 1) * 6.66); setId(false) }}>Next<i className="bi bi-arrow-right ms-1"></i></button>
                 ) : ''}
                 {(page === 15) && (<button className='btn property-footer-button' onClick={finishHandler}>finish<i className="bi bi-arrow-right ms-1"></i></button>)}
